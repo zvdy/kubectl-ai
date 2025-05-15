b2a1a2d222e88ffba25f568d5ca68c3ca33d7bdf
Sunil Arora <arora.sunil@gmail.com>
2025-05-15 10:26:55 -0700

    Added ci job to run evals periodically (#234)

--- dev/ci/periodics/analyze-evals.sh
+++ dev/ci/periodics/analyze-evals.sh
@@ -13,7 +13,6 @@ if [[ -z "${OUTPUT_DIR:-}" ]]; then
     OUTPUT_DIR="${REPO_ROOT}/.build/k8sbench"
     mkdir -p "${OUTPUT_DIR}"
 fi
-echo "Writing results to ${OUTPUT_DIR}"
 
 BINDIR="${REPO_ROOT}/.build/bin"
 mkdir -p "${BINDIR}"
@@ -22,5 +21,5 @@ cd "${REPO_ROOT}/k8s-bench"
 go build -o "${BINDIR}/k8s-bench" .
 
 cd "${REPO_ROOT}"
-"${BINDIR}/k8s-bench" analyze --input-dir "${OUTPUT_DIR}" ${TEST_ARGS:-} -results-filepath k8s-bench.md --output-format markdown
-"${BINDIR}/k8s-bench" analyze --input-dir "${OUTPUT_DIR}" ${TEST_ARGS:-} -results-filepath k8s-bench.json --output-format json
+"${BINDIR}/k8s-bench" analyze --input-dir "${OUTPUT_DIR}" ${TEST_ARGS:-} -results-filepath ${REPO_ROOT}/.build/k8s-bench.md --output-format markdown
+"${BINDIR}/k8s-bench" analyze --input-dir "${OUTPUT_DIR}" ${TEST_ARGS:-} -results-filepath ${REPO_ROOT}/.build/k8s-bench.json --output-format json
