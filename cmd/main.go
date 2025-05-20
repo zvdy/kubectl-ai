59276eece083bf0146026e387a415e059724906d
zvdy <cristian.030902@gmail.com>
2025-05-20 19:51:39 +0200

    test release cicd

--- cmd/main.go
+++ cmd/main.go
@@ -59,6 +59,15 @@ func BuildRootCommand(opt *Options) (*cobra.Command, error) {
 		},
 	}
 
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of kubectl-ai",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("kubectl-ai version: %s\ncommit: %s\ndate: %s\n", version, commit, date)
+			os.Exit(0)
+		},
+	})
+
 	if err := opt.bindCLIFlags(rootCmd.Flags()); err != nil {
 		return nil, err
 	}
